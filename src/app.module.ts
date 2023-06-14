54193a785fc670ca9ed5cc7098288c492a639d9e
Dang Quang Anh <anhdq1102@gmail.com>
2023-06-14 17:59:09 +0700

    init 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './modules/database/database.module';
+import { SubscriptionsModule } from './modules/subscriptions/subscriptions.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), DatabaseModule, SubscriptionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
